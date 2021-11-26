a2f2f21b64d6ba6413baa8ddd74b42c93d76d29d
Richard-0106 <alex19950106@gmail.com>
2021-11-26 13:00:41 +0100

    Módosít gomb

--- admin.js
+++ admin.js
@@ -1,14 +1,50 @@
 $(function () {
- new AjaxHivas().getAjax("http://localhost:3000/termek",window.beolvas,'table',TermekAdmin)
+   let id=0
+    let newAjaxHivas = new AjaxHivas()
+    newAjaxHivas.getAjax("http://localhost:3000/termek", window.beolvas, 'table', TermekAdmin)
     $(window).on("termekTorles", (esemeny) => {
-        console.log("kitörlöm a ",esemeny.detail.nev)
+        console.log("kitörlöm a ", esemeny.detail.nev)
 
-       new AjaxHivas().deleteAjax("http://localhost:3000/termek",esemeny.detail.id)
+        newAjaxHivas.deleteAjax("http://localhost:3000/termek", esemeny.detail.id)
     })
-    $(window).on("termekModositas",(esemeny)=>{
-       // let id= $(this).attr("id")
-        console.log(id)
-    //    new AjaxHivas().putAjax("http://localhost:3000/termek",,esemeny.detail.id)
+    $(window).on("termekModositas", (esemeny) => {
+
+
+        $("#nev").val(esemeny.detail.nev)
+        $("#leiras").val(esemeny.detail.leiras)
+        $("#ar").val(esemeny.detail.ar)
+        id=esemeny.detail.id
+        // let termekNev=adat.nev.val()
+        // let termekLeiras=adat.leiras.val()
+        // let termekAr=adat.ar.val()
+
+
+        // let id= $(this).attr("id")
+        console.log("modosit")
+
+
+    });
+    $("#modosit").on("click", () => {
+    
+        let adat = {
+            nev: $("#nev").val(),
+            leiras: $("#leiras").val(),
+            ar: $("#ar").val()
+        }
+        newAjaxHivas.putAjax("http://localhost:3000/termek", adat, id)
+
 
     })
+    $("#uj-termek").on("click", () => {
+    
+        let adat = {
+            nev: $("#nev").val(),
+            leiras: $("#leiras").val(),
+            ar: $("#ar").val()
+        }
+        newAjaxHivas.postAjax("http://localhost:3000/termek", adat)
+
+
+    })
+
 })
--- ajax.js
+++ ajax.js
@@ -1,52 +1,52 @@
-class AjaxHivas{
-    constructor(){
+class AjaxHivas {
+    constructor() {
 
     }
-    getAjax(fajlnev, myCallback,szulo,osztaly) {
+    getAjax(fajlnev, myCallback, szulo, osztaly) {
         const termekek = [];
         $.ajax({
             url: fajlnev,
-            type:"Get",
-            success: function(result) {
+            type: "Get",
+            success: function (result) {
                 result.forEach((element) => {
                     termekek.push(element);
                 });
-    
-                myCallback(termekek,szulo,osztaly);
+
+                myCallback(termekek, szulo, osztaly);
             },
         });
-    
+
     }
     postAjax(fajlnev, adat) {
         const termekek = [];
         $.ajax({
             url: fajlnev,
-            type:"POST",
-            adat:adat,
-            success: function(result) {        
-                },
-            });
-        }
-        putAjax(fajlnev, adat,id) {
-            const termekek = [];
-            $.ajax({
-                url: fajlnev+"/"+id,
-                type:"PUT",
-                adat:adat,
-                success: function(result) {
-                    
-                    },
-                });
-            }
-            deleteAjax(fajlnev,id) {
-                const termekek = [];
-                $.ajax({
-                    url: fajlnev+"/"+id,
-                    type:"DELETE",
-                    
-                    success: function(result) {
-                        
-                        },
-                    });
-                }
+            type: "POST",
+            data: adat,
+            success: function (result) {
+            },
+        });
     }
+    putAjax(fajlnev, adat, id) {
+        const termekek = [];
+        $.ajax({
+            url: fajlnev + "/" + id,
+            type: "PUT",
+            data: adat,
+            success: function (result) {
+
+            },
+        });
+    }
+    deleteAjax(fajlnev, id) {
+        const termekek = [];
+        $.ajax({
+            url: fajlnev + "/" + id,
+            type: "DELETE",
+
+            success: function (result) {
+
+            },
+        });
+    }
+}
